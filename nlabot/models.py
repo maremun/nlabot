003fda2f9d1e6cd6bca938fa287b983a1a637a1a
maremun <maremun@gmail.com>
2017-10-03 00:26:27 +0300

    fix import datetime

--- nlabot/models.py
+++ nlabot/models.py
@@ -1,6 +1,7 @@
 #   encoding: utf-8
 #   models.py
 
+from datetime import datetime
 from sqlalchemy import Column, DateTime, Integer, String, ForeignKey, \
         create_engine
 from sqlalchemy.ext.declarative import as_declarative
@@ -27,27 +28,27 @@ class User(Base):
 
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, primary_key=True)
     username = Column(String(64), nullable=True)
     first_name = Column(String(64), nullable=True)
     last_name = Column(String(64), nullable=True)
     last_seen_at = Column(DateTime, default=datetime.now, nullable=False)
     # TODO add server_default with timezone
+    # use enum type for states
     #state = Column(String(64), default='start|-1', nullable=False)
 
     student = relationship('Student', use_list=False, back_populates='user')
 
     def __repr__(self):
-        template = '<User[{0:d}] {1:s}>'
-        return template.format(self.id, self.username)
+        return f'<User[{self.user_id}] {self.username}>'
 
 
 class Student(Base):
 
     __tablename__ = 'students'
 
-    id = Column(Integer, primary_key=True)
-    user_id = Column(foreign_key=('users.id'))
+    student_id = Column(Integer, primary_key=True)
+    user_id = Column(foreign_key=('users.user_id'))
     first_name = Column(String(64), nullable=False)
     last_name = Column(String(64), nullable=False)
     avg_grade = Column(Float(32), nullable=True)
@@ -58,8 +59,8 @@ class Student(Base):
                                cascade='all, delete, delete-orphan')
 
     def __repr__(self):
-        template = '<Student[{:d}] {:s} {:s}>'
-        return template.format(self.id, self.first_name, self.last_name)
+        return f'<Student[{self.student_id}] {self.first_name} ' \
+                '{self.last_name}>'
 
 
 class Submission(Base):
@@ -68,11 +69,11 @@ class Submission(Base):
     __table_args__ = (UniqueConstraint('hw_id', 'student_id',
                                        'submission_id'),)
 
-    id = Column(Integer, primary_key=True)
+    submission_id = Column(Integer, primary_key=True)
     # Each submission is a tuple (student_id, hw_id, submission_id) to identify
     # student submitted a work, homework's serial number, submission number
-    # (e.g. Bershatsky submitted his 3rd attempt at solution to hw #3. 
-    student_id = Column(foreign_key=('students.id'))
+    # (e.g. Bershatsky submitted his 3rd attempt at solution to hw #3.
+    student_id = Column(foreign_key=('students.student_id'))
     hw_id = Column(Integer, nullable=False) # HW number
     ordinal = Column(Integer, nullable=False) # Submission number
     submitted_at = Column(DateTime, default=datetime.now(), nullable=False,
@@ -82,7 +83,6 @@ class Submission(Base):
     user = relationship('Student', back_populates='submissions')
 
     def __repr__(self):
-        template = "<Submission[id={:d}] student name='{:s}' HW #'{:d}'" \
-                   "attempt #'{:d}'>"
-        return template.format(self.id, self.student_id, self.hw_id,
-                self.submission_id)
+        return f'<Submission[id={self.submission_id}] student' \
+                'name={self.student_id} HW #{self.hw_id} ' \
+                'attempt #{self.ordinal}>'

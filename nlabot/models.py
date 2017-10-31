95baf2f07dc454a4b20ae94cfc8a14135798cd49
maremun <maremun@gmail.com>
2017-10-31 15:38:11 +0300

    Add homeworks table.

--- nlabot/models.py
+++ nlabot/models.py
@@ -3,7 +3,7 @@
 
 from datetime import datetime
 from sqlalchemy import Column, DateTime, Integer, String, ForeignKey, \
-        Float, create_engine, UniqueConstraint
+        Float, create_engine, UniqueConstraint, ARRAY
 from sqlalchemy.ext.declarative import as_declarative
 from sqlalchemy.dialects.postgresql import ENUM
 from sqlalchemy.orm import scoped_session, sessionmaker, relationship
@@ -77,17 +77,35 @@ class Submission(Base):
     path = Column(String(256))
     # Each submission is a tuple (student_id, hw_id, submission_id) to identify
     # student submitted a work, homework's serial number, submission number
-    # (e.g. Bershatsky submitted his 3rd attempt at solution to hw #3.
+    # (e.g. Student1 submitted his 3rd attempt at solution to hw #3.
     student_id = Column(ForeignKey('students.student_id'))
-    hw_id = Column(Integer, nullable=False)  # HW number
+    hw_id = Column(ForeignKey('homeworks.hw_id'))  # Integer, nullable=False)
     ordinal = Column(Integer, nullable=False)  # Submission number
     submitted_at = Column(DateTime, default=datetime.now(), nullable=False,
                           server_default=text('NOW()'))
     grade = Column(Float(32), nullable=True)  # Grade
 
     student = relationship('Student', back_populates='submissions')
+    homework = relationship('Homework', back_populates='submissions')
 
     def __repr__(self):
         return f'<Submission[id={self.submission_id}] student' \
                 'name={self.student_id} HW #{self.hw_id} ' \
                 'attempt #{self.ordinal}>'
+
+
+class Homework(Base):
+
+    __tablename__ = 'homeworks'
+
+    hw_id = Column(Integer, primary_key=True)
+    total_pts = Column(Integer, nullable=False)
+    n_func = Column(Integer, nullable=False)
+    pts_per_func = Column(ARRAY(Integer), nullable=False)
+
+    submissions = relationship('Submission', back_populates='homework',
+                               cascade='all, delete, delete-orphan')
+
+    def __repr__(self):
+        return f'<Homework[{self.hw_id}] {self.total_pts} ' \
+                '{self.n_func} {self.pts_per_func}>'

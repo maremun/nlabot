388bc36a0f9c29ee9db2c244cdf93376120714f7
maremun <maremun@gmail.com>
2017-11-02 16:31:51 +0300

    Fix registration logic and brush up.

--- nlabot/handlers.py
+++ nlabot/handlers.py
@@ -11,18 +11,24 @@ from .telegram import send_message
 from .utils import check_started, check_registered, download_file
 
 ADDED_STUDENT_TEXT = "Congrats! You have joined the NLA army! SVD bless you!"
+DB_ERROR_TEXT = "Sorry, there has been an internal problem. Please report " \
+                "this to TA."
 ERROR_TEXT = "Sorry, I don't understand. If you are unregistered, please " \
-       "register by sending me a message with your *FirstName " \
-       "LastName*. Otherwise, make a submission (your homework)."
+             "register by sending me a message with your *FirstName " \
+             "LastName*. Otherwise, make a submission (your homework)."
 NAME_FORMAT_TEXT = '*Firstname Lastname*.'
 NOT_ENROLLED_TEXT = "Uh-oh! You seem to be not in the list of enrolled " \
-                       "students, please contact TA to correct this."
+                    "students, please contact TA to correct this."
 NOT_STARTED_TEXT = "Send /start command to interact with me."
 NOT_REGISTERED_TEXT = "You haven't registered yet. Do it by sending me a " \
-                      "message with your *Firstname Lastname*."
+                      "message with your *Firstname Lastname* (as they " \
+                      "appear in Canvas)."
+ON_START_TEXT = "Hello! Please register to submit your homeworks. " \
+                "Send your *FirstName LastName* (as they appear in Canvas)."
 REGISTERED_TEXT = "You've already registered. To reset use /start command."
+RESET_TEXT = "Resetted your registration."
 STARTED_TEXT = "You've already started interaction with me."
-TAKEN_ERROR = "Uh-oh! Somebody took you name! Please report this to your TA!"
+TAKEN_ERROR = "Uh-oh! Somebody took your name! Please report this to your TA!"
 
 
 def handle_update(update, sess, conn, queue):
@@ -38,12 +44,15 @@ def handle_update(update, sess, conn, queue):
     started = check_started(user_id, conn)
     if msg.get('text'):
         if msg['text'].startswith('/start'):
+            prefix = ''
             # STARTING (ADD USER TO users TABLE)
             if started:
+                prefix = RESET_TEXT
                 cursor = conn.execute("""
                     DELETE FROM users
                     WHERE user_id = :user_id
                 """, {'user_id': user_id})
+                conn.commit()
 
             username = msg['from'].get('username')
             first_name = msg['from']['first_name']
@@ -67,8 +76,7 @@ def handle_update(update, sess, conn, queue):
                 """, row)
             conn.commit()
 
-            text = 'Hello! Please register to submit your homeworks. ' \
-                   'Send your *FirstName LastName*.'
+            text = ' '.join((prefix, ON_START_TEXT))
         else:
             # REGISTRATION (CHANGE state TO registered)
             if not started:
@@ -76,44 +84,48 @@ def handle_update(update, sess, conn, queue):
                 send_message(msg['chat']['id'], text, sess=sess)
                 return update['update_id']
 
-            if check_registered(user_id, conn)[0]:
+            if not check_registered(user_id, conn)[0]:
+                # try register
+                split = msg['text'].strip().split(maxsplit=1)
+                if len(split) != 2:
+                    text = NAME_FORMAT_TEXT
+                    send_message(msg['chat']['id'], text, sess=sess)
+                    return update['update_id']
+
+                first_name, last_name = split
+                row = {'user_id': user_id, 'first_name': first_name,
+                       'last_name': last_name}
+                try:
+                    cursor = conn.execute("""
+                        UPDATE users
+                        SET state = 'registered',
+                            student_id = students.student_id
+                        FROM students
+                        WHERE user_id = :user_id AND students.first_name =
+                        :first_name AND students.last_name = :last_name
+                        RETURNING students.student_id;
+                    """, row)
+                    conn.commit()
+                    result = cursor.first()
+                    if result is None:
+                        text = NOT_ENROLLED_TEXT
+                    else:
+                        text = ADDED_STUDENT_TEXT
+
+                # oops, smbd took your place already
+                except IntegrityError:
+                    text = TAKEN_ERROR
+                    conn.rollback()
+
+                # smth wrong
+                except Exception as e:
+                    logging.error(e)
+                    text = DB_ERROR_TEXT
+                    conn.rollback()
+            else:
                 text = REGISTERED_TEXT
                 send_message(msg['chat']['id'], text, sess=sess)
                 return update['update_id']
-
-            split = msg['text'].strip().split(maxsplit=1)
-            if len(split) != 2:
-                text = NAME_FORMAT_TEXT
-                send_message(msg['chat']['id'], text, sess=sess)
-                return update['update_id']
-
-            first_name, last_name = split
-            row = {'user_id': user_id, 'first_name': first_name,
-                   'last_name': last_name}
-            try:
-                cursor = conn.execute("""
-                    UPDATE users
-                    SET state = 'registered', student_id = students.student_id
-                    FROM students
-                    WHERE user_id = :user_id AND students.first_name =
-                    :first_name AND students.last_name = :last_name
-                    RETURNING students.student_id;
-                """, row)
-                conn.commit()
-                result = cursor.first()
-                if result is None:
-                    text = NOT_ENROLLED_TEXT
-                else:
-                    text = ADDED_STUDENT_TEXT
-
-            except IntegrityError:
-                text = TAKEN_ERROR
-                conn.rollback()
-
-            except Exception as e:
-                logging.error(e)
-                text = ERROR_TEXT
-                conn.rollback()
 
     elif msg.get('document'):
         if not started:
@@ -134,7 +146,6 @@ def handle_update(update, sess, conn, queue):
                                                      hw_id,
                                                      filepath,
                                                      chat_id))
-
     else:
         text = ERROR_TEXT
 

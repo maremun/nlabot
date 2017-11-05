8336a96f95094506497af738185eacbcd27bd00b
maremun <maremun@gmail.com>
2017-11-06 00:58:29 +0300

    Fix test checker.

--- nlabot/checker.py
+++ nlabot/checker.py
@@ -2,7 +2,6 @@
 #   checker.py
 
 from collections import Callable
-from functools import wraps
 from io import StringIO
 from traceback import print_exc
 
@@ -45,55 +44,39 @@ class TestChecker(IChecker):
         super(TestChecker, self).__init__(module)
 
     def check(self):
+        foo_cases = [(None, 42)]
+        bar_cases = [(2, 4), (3, 9), (5, 25)]
+        zoo_cases = [(2, 4), (3, 9), (4, 42)]
+
         results = [
-            self.check_foo(),
-            self.check_bar(),
-            self.check_zoo(),
+            self.test('foo', foo_cases),
+            self.test('bar', bar_cases),
+            self.test('zoo', zoo_cases),
         ]
         return results
 
-    foo_cases = [(None, 42)]
-    bar_cases = [(2, 4), (3, 9), (5, 25)]
-    zoo_cases = [(2, 4), (3, 9), (4, 42)]
+    def test(self, name, cases):
+        report = self.make_report(name)
+        if cases is None:
+            report['pass'] = [0]  # no cases provided
+        else:
+            report['pass'] = [0] * len(cases)
+        if not report['exists']:
+            return report
 
-    def check_dec(name, cases=None):
-        def wrap(f):
-            @wraps(f)
-            def wrapper(*args, **kwargs):
-                report = args[0].make_report(name)
-                if cases is None:
-                    report['pass'] = [0]  # no cases provided
+        func = getattr(self.nb, name)
+        try:
+            for i, (argument, result) in enumerate(cases):
+                if argument is None:
+                    if result == func():
+                        report['pass'][i] = 1
                 else:
-                    report['pass'] = [0] * len(cases)
-                if not report['exists']:
-                    return report
-                try:
-                    for i, (argument, result) in enumerate(cases):
-                        if argument is None:
-                            if result == f(*args, **kwargs)():
-                                report['pass'][i] = 1
-                        else:
-                            if result == f(*args, **kwargs)(argument):
-                                report['pass'][i] = 1
-                except Exception as e:
-                    fout = StringIO()
-                    print_exc(file=fout)
-                    report['exc_info'] = fout.getvalue()
-                    fout.close()
-                finally:
-                    return report
-
-            return wrapper
-        return wrap
-
-    @check_dec('foo', foo_cases)
-    def check_foo(self):
-        return self.nb.foo
-
-    @check_dec('bar', bar_cases)
-    def check_bar(self):
-        return self.nb.bar
-
-    @check_dec('zoo', zoo_cases)
-    def check_zoo(self):
-        return self.nb.zoo
+                    if result == func(argument):
+                        report['pass'][i] = 1
+        except Exception as e:
+            fout = StringIO()
+            print_exc(file=fout)
+            report['exc_info'] = fout.getvalue()
+            fout.close()
+        finally:
+            return report
